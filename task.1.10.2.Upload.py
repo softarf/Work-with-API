6892ff9b130194c9005d1315a5317c4c771dfa12
softarf <98207887+softarf@users.noreply.github.com>
2022-12-18 22:07:53 +0500

    Детализирует вторую задачу из Д.З. 10
    Разбивает основную задачу на три шага (три отдельных метода),
    конкретизируя (выделяя) отдельные действия.

--- task.1.10.2.Upload.py
+++ task.1.10.2.Upload.py
@@ -19,27 +19,40 @@ class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
-    def upload(self, file_path: str, file_name: str):
-        """Метод загружает файл в папку file_path на яндекс диск"""
-        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"  # из документации
+    def choose_data_straem(self, file_name: str):
+        """Метод определяет источник данных для отправки на Я.Диск"""
+        self.data_straem = open(file_name, 'rb')
+
+    def get_href(self, file_path: str):
+        """Метод получает ссылку для загрузки данных"""
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"  # из документации по Я.Диск
         my_params = {"path": file_path, "overwrite": "true"}
         my_headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
-        #  Получает ссылку для загрузки файла
-        my_href = requests.get(upload_url, params=my_params, headers=my_headers).json().get("href", "")
-        #  Добавляет файл на Яндекс Диск
-        my_response = requests.put(my_href, data=open(file_name, 'rb'))
-        # Функция может ничего не возвращать
+        self.href = requests.get(url=upload_url, params=my_params, headers=my_headers).json().get("href", "")
+
+    def upload_file(self) -> str:
+        """Метод отправляет файл на Яндекс Диск"""
+        my_response = requests.put(url=self.href, data=self.data_straem)
+        # Функция может ничего не возвращать (из лекции)
         if 200 <= my_response.status_code < 300:
-            print("'Success'")  # "Успешно"
+            return "'Success'"  # "Успешно"
+        return "'Что-то не так: Status Code " + str(my_response.status_code) + "'"  # "Ошибка"
+
+def main():
+    # Получает путь к загружаемому файлу и токен от пользователя: test_05-15.txt
+    my_file = input_file_name()
+    TOKEN = input_token()
+    uploader = YaUploader(TOKEN)
+    uploader.choose_data_straem(my_file)
+    path_to_file = 'disk:/' + os.path.basename(my_file)
+    uploader.get_href(path_to_file)
+    res = uploader.upload_file()
+    print(res)
 
 
 if __name__ == '__main__':
-    # Получает путь к загружаемому файлу и токен от пользователя: test_05-15.txt
-    file_name = input_file_name()
-    TOKEN = input_token()
-    uploader = YaUploader(TOKEN)
-    path_to_file = 'disk:/' + os.path.basename(file_name)
-    uploader.upload(path_to_file, file_name)
+    main()
+
 
 print('\n  -- Конец --  ')  # - Для блокнота
 # input('\n  -- Конец --  ')	#	Типа  "Пауза" - Для среды
