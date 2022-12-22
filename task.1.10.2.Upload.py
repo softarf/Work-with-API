e75cb791465385cae9ad2a924576974485f70d9f
softarf <98207887+softarf@users.noreply.github.com>
2022-12-23 02:17:57 +0500

    Аннотирует атрибуты во второй задаче Д.З. 10

--- task.1.10.2.Upload.py
+++ task.1.10.2.Upload.py
@@ -1,58 +1,62 @@
 # -*- coding: utf-8 -*-
 #                        Задача №2. Сохранить указанный файл на Яндекс.Диске.
 #
+from typing import BinaryIO, Optional
+
 import os
 import requests
 
 
-def input_file_name():
+def input_file_name() -> str:
     return input()
 
 
-def input_token():
-    my_token = input()
+def input_token() -> str:
+    my_token: str = input()
+    # или можно считать из файла ...
     return my_token
 
 
 class YaUploader:
 
-    def __init__(self, token: str):
-        self.token = token
+    def __init__(self, token: str) -> None:
+        self.token: str = token
+        self.data_stream: Optional[BinaryIO] = None
+        self.href: str = ''
 
-    def choose_data_straem(self, file_name: str):
+    def choose_data_stream(self, file_name: str) -> None:
         """Метод определяет источник данных для отправки на Я.Диск"""
-        self.data_straem = open(file_name, 'rb')
+        self.data_stream = open(file_name, 'rb')
 
-    def get_href(self, file_path: str):
+    def get_href(self, file_path: str) -> None:
         """Метод получает ссылку для загрузки данных"""
-        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"  # из документации по Я.Диск
-        my_params = {"path": file_path, "overwrite": "true"}
-        my_headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
+        upload_url: str = 'https://cloud-api.yandex.net/v1/disk/resources/upload'  # из документации по Я.Диск
+        my_params: dict = {'path': file_path, 'overwrite': 'true'}
+        my_headers: dict = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
         self.href = requests.get(url=upload_url, params=my_params, headers=my_headers).json().get("href", "")
 
     def upload_file(self) -> str:
         """Метод отправляет файл на Яндекс Диск"""
-        my_response = requests.put(url=self.href, data=self.data_straem)
+        my_response = requests.put(url=self.href, data=self.data_stream)
         # Функция может ничего не возвращать (из лекции)
         if 200 <= my_response.status_code < 300:
             return "'Success'"  # "Успешно"
-        return "'Что-то не так: Status Code " + str(my_response.status_code) + "'"  # "Ошибка"
+        return f"'Что-то не так: Status Code {my_response.status_code}'"  # "Ошибка"
 
-def main():
-    # Получает путь к загружаемому файлу и токен от пользователя: test_05-15.txt
-    my_file = input_file_name()
-    TOKEN = input_token()
+
+def main() -> None:
+    # Получает от пользователя путь к загружаемому файлу (test_05-15.txt) и токен.
+    my_file: str = input_file_name()
+    TOKEN: str = input_token()
     uploader = YaUploader(TOKEN)
-    uploader.choose_data_straem(my_file)
-    path_to_file = 'disk:/' + os.path.basename(my_file)
+    uploader.choose_data_stream(my_file)
+    path_to_file: str = 'disk:/' + os.path.basename(my_file)
     uploader.get_href(path_to_file)
-    res = uploader.upload_file()
+    res: str = uploader.upload_file()
     print(res)
 
 
 if __name__ == '__main__':
     main()
-
-
-print('\n  -- Конец --  ')  # - Для блокнота
-# input('\n  -- Конец --  ')	#	Типа  "Пауза" - Для среды
+    # input('\n  -- Конец --  ')    #	Типа  "Пауза" - Для среды
+    # print('\n  -- Конец --  ')  # - Для блокнота
